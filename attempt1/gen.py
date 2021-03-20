6cad4b697cac3bbbe6b5589d768f0621ead6ef12
alcofrisbas <benjamin.k.greene@gmail.com>
2021-03-20 17:18:00 +0000

    added step generator and thought about testing...

---
+++ attempt1/gen.py
@@ -0,0 +1,29 @@
+class step:
+    def __init__(self,half=False):
+        self.steps = (0b1000,
+             0b1100,
+             0b0100,
+             0b0110,
+             0b0010,
+             0b0011,
+             0b0001,
+             0b1001)
+        if half:
+            self.step = self.steps[::2]
+        self.n = 0
+        self.l = len(self.steps)
+        self.direction = 1
+
+    def forward(self):
+        self.direction = 1
+    def backward(self):
+        self.direction = -1
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        out = self.steps[self.n]
+        self.n = (self.n + self.direction) % self.l
+        return out
+
--- attempt1/server.py
+++ attempt1/server.py
@@ -2,10 +2,21 @@ import multiprocessing
 import queue
 import time
 import smbus
+from gen.py import step
 
-def 8bit(q, bus):
+def Eightbit(q, bus, dev,reg):
+    s1 = step()
+    s2 = step()
     while True:
         try:
             item = q.get(False)
         except queue.Empty:
             pass
+
+if __name__ == "__main__":
+    g = step()
+    for i in range(4):
+        print(next(g))
+    g.backward()
+    for i in range(4):
+        print(next(g))
